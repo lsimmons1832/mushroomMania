e2acacf1397275401e9c6fa840514adf2c33ff85
Latasha Simmons <lsimmons1832@gmail.com>
2017-05-17 18:36:35 -0500

    assignment complete

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,38 @@ app.run((FIREBASE_CONFIG) =>{
  firebase.initializeApp(FIREBASE_CONFIG);
 });
 
-app.controller("ItemCtrl", ($scope) =>{
+app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) =>{
+	$scope.showListView = true;
+  $scope.mushroomItems = [];
+  $scope.searchText = "";
+
+	let getMushrooms = () =>{  //This is the function getting info from Firebase
+		let mushrooms = []; //This itemz array only exist in this function
+		return $q((resolve, reject) =>{
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/mushrooms.json`)
+			.then((fbItems)=>{
+          var itemCollection = fbItems.data;
+          Object.keys(itemCollection).forEach((key) => {
+            itemCollection[key].id=key;
+            mushrooms.push(itemCollection[key]);
+          });
+          resolve(mushrooms);
+			}).catch((error) =>{
+				reject(error);
+			});
+		});
+	};
+
+		let getShrooms = () => {
+			getMushrooms().then((shrooms) =>{
+				$scope.mushroomItems = shrooms;
+			}).catch((error) =>{
+				console.log("get Error", error);
+			});
+		};
+
+		getShrooms();
+
+
 
 });
